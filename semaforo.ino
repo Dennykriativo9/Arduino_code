bd82186f06dc64b72e12747d089182877071d753
Denny Salgueiro <147348390+Dennykriativo9@users.noreply.github.com>
2025-12-19 11:05:35 +0100

    Add files via upload
    codigo de teste para o modulo Semafero!

---
+++ semaforo.ino
@@ -0,0 +1,28 @@
+#define LED_BUILTIN1 4
+#define LED_BUILTIN2 5
+#define LED_BUILTIN3 6
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN1, OUTPUT);
+  pinMode(LED_BUILTIN2, OUTPUT);
+  pinMode(LED_BUILTIN3, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN1, HIGH);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN2, LOW);
+  digitalWrite(LED_BUILTIN3, LOW);
+  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN1,LOW);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN2, HIGH);
+  digitalWrite(LED_BUILTIN3, LOW);
+  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN1, LOW);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN2, LOW);
+  digitalWrite(LED_BUILTIN3, HIGH);
+  delay(1000); 
+}
